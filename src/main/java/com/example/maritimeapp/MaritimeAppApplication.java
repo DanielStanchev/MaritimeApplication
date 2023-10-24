8f54160dd680112f84a1f9510ca74e60de360f83
Daniel Stanchev <danyto0o@mail.bg>
2023-10-24 16:01:57 +0300

    Initial commit

---
+++ src/main/java/com/example/maritimeapp/MaritimeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.maritimeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaritimeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaritimeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maritimeapp/MaritimeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maritimeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaritimeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
