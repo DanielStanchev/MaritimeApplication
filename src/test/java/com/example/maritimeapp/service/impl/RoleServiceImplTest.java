5522d5d3f2628c7d77e90ba67af6e683839682b0
Daniel Stanchev <danyto0o@mail.bg>
2023-11-22 17:29:16 +0200

    commit tests fix

--- src/test/java/com/example/maritimeapp/service/impl/RoleServiceImplTest.java
+++ src/test/java/com/example/maritimeapp/service/impl/RoleServiceImplTest.java
@@ -2,7 +2,6 @@ package com.example.maritimeapp.service.impl;
 
 import com.example.maritimeapp.model.entity.RoleEntity;
 import com.example.maritimeapp.repository.RoleRepository;
-import com.example.maritimeapp.service.impl.RoleServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -25,17 +24,14 @@ import static org.mockito.Mockito.when;
 
     @Test
     void testFindAll() {
-        // Initialize the mocks
+
         MockitoAnnotations.openMocks(this);
 
-        // Mock the behavior of roleRepository.findAll
         List<RoleEntity> expectedRoles = Arrays.asList(new RoleEntity(), new RoleEntity());
         when(roleRepository.findAll()).thenReturn(expectedRoles);
 
-        // Call the findAll method
         List<RoleEntity> result = roleService.findAll();
 
-        // Verify that findAll returns the expected list of roles
         assertEquals(expectedRoles, result);
     }
 }
